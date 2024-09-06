9f6d6f32f83c06da2b32d63ccda5a640b72c73e4
AndreFelipe1D <114994513+AndreFelipe1D@users.noreply.github.com>
2024-09-06 20:21:02 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,107 @@
+export const perguntas = [
+    {
+        enunciado: "Entre um Civic SI 2.0 2008 ou Opala Comodoro 4.1 1992, Qual você escolheria?",
+        alternativas: [
+            {
+                texto: "Civic SI 2008",
+                afirmacao: [
+                    "Se você escolheu o Civic, foi uma Ótima escolha!O Civic com seu sistema I-Vtec e os 192 pocotós anda muito bem. ",
+                    "Já que não estamos no Japão, nada mais racional do que trazer um pouco do Japão para o Brasil."
+            ],
+            proxima:1,
+            },
+            {
+             texto: "Opala 4.1 1992",   
+             afirmacao: [
+                "Se você escolheu o Opala foi uma ótima escolha! Um ícone automotivo brasileiro que por onde passa chama a atenção.",
+                "Classe e Robustez em um carro só, isso é Opala" 
+            ],
+            proxima:2,
+            }
+        ]
+    },
+    {
+        enunciado: "Com Carburador ou Injeção programável?",
+        alternativas: [
+            {
+                texto: "Com Carburador",
+                afirmacao: [
+                    "Ousado em escolher o carburador, hoje em dia é dificil ver carros com essa configuração.",
+                    "Prepara o bolso que carburado gasta mais combustivel."
+            ],
+            proxima:3,
+            },
+            {
+                texto: "Com Injeção programável",
+                afirmacao: [
+                    "Escolheu bem,a escolha de uma injeção programável possibilita uma economia melhor de combustível.",
+                    "Anderson Dick agradece a sua escolha."
+            ],
+            }
+            
+        ]
+    },
+    {
+        enunciado: "Turbo ou Aspirado?",
+        alternativas: [
+            {
+                texto: "Turbo",
+                afirmacao:[
+                    "Se escolheu turbo se prepara porque quando a turbina encher é só uma patada de potência de uma vez.",
+                    "Se é eficiência é o que você busca, você está no caminho certo."
+            ],
+            proxima:4,
+            },
+            {
+                texto: "Aspirado",
+                afirmacao: [
+                    "Se fazer barulho por onde passa for o objetivo, os aspirados são a escolha certa.",
+                    "É barulho e diversão a vontade."
+            ],
+            proxima:5,
+            }
+        ]
+    },
+    {
+        enunciado: "Correr à noite em Le Mans ou em Nurburgring?",
+        alternativas: [
+            {
+                texto:"Le Mans",
+                afirmacao:[
+                    "Essa foi uma escolha racional, pois em Le Mans pelo menos tem iluminação",
+                    "Um circuto marcante na história do automobilismo, boa escolha."
+            ],
+            proxima:6,
+            },
+            {
+                texto:"Nurburgring",
+                afirmacao:[
+                    "Se quiser conhecer o Ayrton Senna de perto Nurburgring é a melhor escolha",
+                    "Circuito incrível, mas a noite deve ser tenso, escolha ousada."
+            ],
+            proxima:7,
+            }
+        ]
+    },
+    {
+        enunciado: "Qual o Melhor Motor para o Skyline R34 do Brian O'Conner?",
+        alternativas: [
+            {
+                texto: "Gallo 12",
+                afirmacao:[
+                    "Você não conhece o filme ou se esqueceu da clássica cena.",
+                    "Não sabia que pizzaria fazia motor de carro."
+            ],
+
+            },
+            {
+                texto:"Gallo 24",
+                afirmacao:[
+                    "Tá sabendo hein, entendeu a referência, isso aí!",
+                    "Não sabia que pizzaria fazia motor de carro."
+            ],
+
+            }
+        ]
+    },
+];
